2e0a8b15b5b415b7f95f5fcd343a1fd30b267f68
tanuakozyrek <kozyrektt@gmail.com>
2025-07-15 15:54:55 +0300

    Update d2_inflation_nburate_dag.py

--- dags/d2_inflation_nburate_dag.py
+++ dags/d2_inflation_nburate_dag.py
@@ -7,7 +7,7 @@ from sqlalchemy import create_engine
 from functools import wraps
 
 # 1.Підключення до PostgreSQL
-db_url = "postgresql+psycopg2://tanykozyr2025:foS_*529gpx@postgresql-tanykozyr2025.alwaysdata.net:5432/tanykozyr2025_postsql"
+db_url = "..."
 engine = create_engine(db_url)
 #--------------------------------------------------------------------------------------------------------------
 # 2.Декоратор логування
